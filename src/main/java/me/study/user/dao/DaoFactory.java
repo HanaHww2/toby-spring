1f14772b0031c4d83cb826d6b63b9018d2baaecf
HanahHwang <hanahww2@gmail.com>
2021-10-22 02:42:38 +0900

    refactor: spring 프레임워크 적용
    스프링 애플리케이션 컨텍스트를 활용해 IoC를 적용

--- src/main/java/me/study/user/dao/DaoFactory.java
+++ src/main/java/me/study/user/dao/DaoFactory.java
@@ -1,23 +1,19 @@
 package me.study.user.dao;
 
-/*
- * 오브젝트 팩토리 클래스를 이용한 제어의 역전 적용
- * dao와 connectionMaker 구현 클래스 간
- * 런타임 오브젝트 의존 관계를 설정하는 책임을 갖는다.
- * 즉, dao 클래스 객체의 생성 방법을 결정하고 객체를 반환한다.
- * */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 설정 정보
 public class DaoFactory {
+    @Bean // 오브젝트 생성을 담당하는 IoC용 메소드를 가리키는 어노테이션
     public UserDao userDao() {
-        UserDao userDao = new UserDao(connectionMaker());// 생성자 파라미터로 오브젝트 전달, 런타임 의존 관계 주입
+        UserDao userDao = new UserDao(connectionMaker());// 생성자 파라미터로 오브젝트 전달, 런타임 의존 관계 설정
         return userDao;
-    }
-    public SampleDao sampleDao() {
-        SampleDao sampleDao = new SampleDao(connectionMaker());
-        return sampleDao;
     }
     /*
     * Dao가 많아지는 경우 발생할 중복 문제 해결을 위해 메소드 분리
     * */
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- src/main/java/me/study/user/dao/UserDaoTest.java
+++ src/main/java/me/study/user/dao/UserDaoTest.java
@@ -1,6 +1,8 @@
 package me.study.user.dao;
 
 import me.study.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,10 +11,12 @@ import java.sql.SQLException;
 */
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class); // 두번째 매개변수로 제네릭 타입을 이용한 반환 타입 지정
 
         User user = new User();
-        user.setId("test2");
+        user.setId("test5");
         user.setName("하나");
         user.setPassword("password");
 
