2cc553fe13deeb21dc79ae81e7f02e9d94fc8787
HanaHww2 <hanahww2@gmail.com>
2021-12-06 21:57:33 +0900

    feat: 의존관계 검색을 구현하는 UserDao 생성자 추가

--- src/main/java/me/study/user/dao/UserDao.java
+++ src/main/java/me/study/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package me.study.user.dao;
 
 import me.study.user.domain.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.*;
 
@@ -13,6 +14,22 @@ public class UserDao {
     // 생성자 파라미터를 이용해 외부에서 오브젝트를 주입받는다.(의존관계 생성)
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
+    }
+
+    // 의존관계 검색(DL)을 이용하는 생성자
+    public UserDao() {
+
+        /*
+        * 외부 컨테이너를 이용해 다이내믹하게 런타임 의존관계를 맺는다, 하지만 외부에서 주입되는 형태가 아닌 직접 요청하는 방식이다.
+        DaoFactory daoFactory = new DaoFactory();
+        this.connectionMaker = daoFactory.connectionMaker();
+        */
+
+        /*
+        * 위와 달리, 스프링 컨테이너를 이용하면 의존관계 검색을 통해 의존관계를 주입받게 된다.
+        * */
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
