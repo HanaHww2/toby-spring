4b2fa1dbd1c3f68371816d43d57667425a8a646c
HanaHww2 <hanahww2@gmail.com>
2022-01-01 18:00:12 +0900

    refactor: xml파일을 이용해 애플리케이션 컨텍스트 생성
    xml을 이용해 의존관계 주입 정보를 스프링 컨텍스트에
    전달한다.

---
+++ src/main/java/me/study/user/dao/UserDaoXmlConfigTest.java
@@ -0,0 +1,30 @@
+package me.study.user.dao;
+
+import me.study.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoXmlConfigTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class); // 두번째 매개변수로 제네릭 타입을 이용한 반환 타입 지정
+
+        User user = new User();
+        user.setId("test6");
+        user.setName("하나");
+        user.setPassword("password");
+
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+
+    }
+}
